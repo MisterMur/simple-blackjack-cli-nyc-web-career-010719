2ab751c84ba7e028a377c7f43bcc4b6404e355ba
Brian Murillo <brian.j.murillo@gmail.com>
2018-12-16 23:28:19 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -20,10 +20,12 @@ end
 
 def get_user_input
   # code #get_user_input here
+  gets
 end
 
-def end_game
+def end_game(num)
   # code #end_game here
+  "Sorry, you hit #{num}. Thanks for playing!"
 end
 
 def initial_round
